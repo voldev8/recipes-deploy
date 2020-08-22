76aa859b6f58c8b255128833346cb4be861fa75c
Damon <voldev@hotmail.com>
2020-08-22 16:47:11 -0400

    default engine error fix second try

--- app.js
+++ app.js
@@ -52,13 +52,13 @@ app.use(function (err, req, res, next) {
 });
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+);
+// }
 
 module.exports = app;
