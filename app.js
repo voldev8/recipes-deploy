178f81e03ca0bacfc5400b9e3236db1b72fe1d36
Damon <voldev@hotmail.com>
2020-08-22 16:37:30 -0400

    default engine error fix second try

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/recipes', recipesRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
