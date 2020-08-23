436d3cc9297afc965c124c0a371220b272d0617d
Damon <voldev@hotmail.com>
2020-08-23 14:54:49 -0400

    .

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + 'client', 'build', 'index.html'));
 });
 
