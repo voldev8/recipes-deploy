f171bd6a4c94292f9d06a75ede02d1ef8c92c8f5
Damon <voldev@hotmail.com>
2020-08-23 14:54:32 -0400

    .

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client', 'build', 'index.html'));
 });
 
 app.use('/api/recipes', recipesRouter);
